e7b459dd65367464b0362d7eb6ada548b0da0dfb
Álvaro Quirós <alvaro.quiros@gmail.com>
2017-06-20 10:43:10 +0200

    Fix eslint errors

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 "use strict";
 
-var TelegramBot = require('node-telegram-bot-api');
+var TelegramBot = require("node-telegram-bot-api");
 
 var Execution = global.ExecutionClass;
 
@@ -19,17 +19,15 @@ class confirmExecutor extends Execution {
     let cancelButtonMessage = params.cancel_button_message;
     let confirmMessage = params.confirm_message;
     let cancelMessage = params.cancel_message;
-    let action_on_timeout = params.action_on_timeout;
     var authorizedUsers = params.authorized_users;
-    var timeout = params.timeout;
     var authorized = true;
 
     // Keyboard buttons with the options
     var options = {
       reply_markup: JSON.stringify({
         inline_keyboard: [
-          [{ text: confirmButtonMessage, callback_data: '1' }],
-          [{ text: cancelButtonMessage, callback_data: '2' }]
+          [{ text: confirmButtonMessage, callback_data: "1" }],
+          [{ text: cancelButtonMessage, callback_data: "2" }]
         ]
       })
     };
@@ -43,7 +41,7 @@ class confirmExecutor extends Execution {
     });
 
     // Start polling waiting for the answer
-    _this.bot.on('callback_query', function (msg) {
+    _this.bot.on("callback_query", function (msg) {
       let back_message_id = msg.message.message_id;
 
       // Checks if the user who answered is in the authorized list if the param is received
@@ -59,10 +57,10 @@ class confirmExecutor extends Execution {
       // check if the response belongs to the same messageId
       if (back_message_id == _this.opts.message_id) {
         if (authorized) {
-          if (msg.data == '1') {
+          if (msg.data == "1") {
 
             //Send the confirmation message with the name of the user who confirmed
-            confirmMessage = confirmMessage + ' - ' + msg.from.first_name + ' ' + msg.from.last_name;
+            confirmMessage = confirmMessage + " - " + msg.from.first_name + " " + msg.from.last_name;
             _this.bot.sendMessage(chatId, confirmMessage);
 
             //Stop polling
@@ -76,13 +74,13 @@ class confirmExecutor extends Execution {
           } else {
 
             let endOptions = {
-              end: 'error',
-              messageLog: 'Chain stop forced by user ' + msg.from.first_name + ' ' + msg.from.last_name,
-              execute_err_return: 'Chain stop forced by user ' + msg.from.first_name + ' ' + msg.from.last_name
-            }
+              end: "error",
+              messageLog: "Chain stop forced by user " + msg.from.first_name + " " + msg.from.last_name,
+              execute_err_return: "Chain stop forced by user " + msg.from.first_name + " " + msg.from.last_name
+            };
 
             //Send the cancellation message with the name of the user who cancelled
-            cancelMessage = cancelMessage + ' - ' + msg.from.first_name + ' ' + msg.from.last_name;
+            cancelMessage = cancelMessage + " - " + msg.from.first_name + " " + msg.from.last_name;
             _this.bot.sendMessage(chatId, cancelMessage);
 
             //Stop polling
@@ -95,7 +93,7 @@ class confirmExecutor extends Execution {
             _this.end(endOptions);
           }
         } else {
-          let unauthorizedMessage = 'Sorry ' + msg.from.first_name + ', you are not authorized for the action'
+          let unauthorizedMessage = "Sorry " + msg.from.first_name + ", you are not authorized for the action";
           _this.bot.sendMessage(chatId, unauthorizedMessage);
         }
       }
@@ -106,18 +104,14 @@ class confirmExecutor extends Execution {
     var _this = this;
     let chatId = params.chatId;
     let message = params.message;
-    let confirmButtonMessage = params.confirm_button_message;
-    let cancelButtonMessage = params.cancel_button_message;
-    let confirmMessage = params.confirm_message;
-    let cancelMessage = params.cancel_message;
     let action_on_timeout = params.action_on_timeout;
 
     // If the reason is timeout and the action_on_timeout is ok the process ends ok
-    if (reason == 'timeout' && action_on_timeout == 'ok'){
+    if (reason == "timeout" && action_on_timeout == "ok"){
       _this.bot.stopPolling();
 
       //Send the confirmation message with the name of the user who confirmed
-      let confirmMessage = 'Process timeout, action on timeout: ' + action_on_timeout + ', the chain continues';
+      let confirmMessage = "Process timeout, action on timeout: " + action_on_timeout + ", the chain continues";
       _this.bot.sendMessage(chatId, confirmMessage);
 
       //Edit the message erasing the buttons
@@ -128,10 +122,10 @@ class confirmExecutor extends Execution {
     }else{
       // Any other reason or no action defined on timeout the process ends with error
       let endOptions = {
-        end: 'error',
-        messageLog: 'Process killed, reason: ' + reason,
-        execute_err_return: 'Process killed, reason: ' + reason
-        }
+        end: "error",
+        messageLog: "Process killed, reason: " + reason,
+        execute_err_return: "Process killed, reason: " + reason
+      };
 
       //Stop polling
       _this.bot.stopPolling();
@@ -140,7 +134,7 @@ class confirmExecutor extends Execution {
       _this.bot.editMessageText(message, _this.opts);
 
       //Send the cancellation message with the name of the user who cancelled
-      let cancelMessage = 'Procces killed, reason: ' + reason + ', acation on timeout: '+ action_on_timeout + ', the chain stops.';
+      let cancelMessage = "Procces killed, reason: " + reason + ", acation on timeout: "+ action_on_timeout + ", the chain stops.";
       _this.bot.sendMessage(chatId, cancelMessage);
 
       //Process end error
