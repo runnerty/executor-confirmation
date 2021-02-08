3f716295569ef31e06b6cddfb44afd44ad370dfe
Carlos Carmona <carlos@coderty.com>
2021-02-08 09:26:52 +0100

    3.0.0 (#2)
    * build: 3.0.0

--- README.md
+++ README.md
@@ -1,8 +1,45 @@
+<p align="center">
+  <a href="http://runnerty.io">
+    <img height="257" src="https://runnerty.io/assets/header/logo-stroked.png">
+  </a>
+  <p align="center">A new way for processes managing</p>
+</p>
+
+[![NPM version][npm-image]][npm-url] [![Downloads][downloads-image]][npm-url] [![Dependency Status][david-badge]][david-badge-url]
+<a href="#badge">
+<img alt="code style: prettier" src="https://img.shields.io/badge/code_style-prettier-ff69b4.svg">
+</a>
+
 # Confirm executor for [Runnerty]:
 
-Executor for Runnerty integrated with Telegram. It sends a message to your Telegram chat and waits for an answer to continue or stop the chain. 
+Executor for Runnerty integrated with Telegram. It sends a message to your Telegram chat and waits for an answer to continue or stop the chain.
+
+### Installation:
+
+Through NPM
+
+```bash
+npm i @runnerty/executor-confirmation
+```
+
+You can also add modules to your project with [runnerty-cli]
+
+```bash
+npx runnerty-cli add @runnerty/executor-confirmation
+```
+
+This command installs the module in your project, adds example configuration in your `config.json` and creates an example plan of use.
+
+If you have installed [runnerty-cli] globally you can include the module with this command:
+
+```bash
+rty add @runnerty/executor-confirmation
+```
 
 ### Configuration sample:
+
+Add in [config.json]:
+
 ```json
 {
   "id": "confirmation_default",
@@ -13,11 +50,14 @@ Executor for Runnerty integrated with Telegram. It sends a message to your Teleg
 ```
 
 ### Plan sample:
+
+Add in [plan.json]:
+
 ```json
 {
-  "id":"confirmation_default",
-  "message":"Message" ,
-  "confirm_button_message":"ok button message",
+  "id": "confirmation_default",
+  "message": "Message",
+  "confirm_button_message": "ok button message",
   "cancel_button_message": "cancel button message",
   "confirm_message": "confirmation message",
   "cancel_message": "cancel message"
@@ -40,4 +80,12 @@ There is also an option to include authorized users. Telegram's user name or Id 
 }
 ```
 
-[Runnerty]: http://www.runnerty.io
+[runnerty]: http://www.runnerty.io
+[downloads-image]: https://img.shields.io/npm/dm/@runnerty/executor-confirmation.svg
+[npm-url]: https://www.npmjs.com/package/@runnerty/executor-confirmation
+[npm-image]: https://img.shields.io/npm/v/@runnerty/executor-confirmation.svg
+[david-badge]: https://david-dm.org/runnerty/executor-confirmation.svg
+[david-badge-url]: https://david-dm.org/runnerty/executor-confirmation
+[config.json]: http://docs.runnerty.io/config/
+[plan.json]: http://docs.runnerty.io/plan/
+[runnerty-cli]: https://www.npmjs.com/package/runnerty-cli
--- index.js
+++ index.js
@@ -1,53 +1,52 @@
-"use strict";
+'use strict';
 
-var TelegramBot = require("node-telegram-bot-api");
+const telegramBot = require('node-telegram-bot-api');
 
-var Execution = global.ExecutionClass;
+const Executor = require('@runnerty/module-core').Executor;
 
-class confirmExecutor extends Execution {
+class confirmExecutor extends Executor {
   constructor(process) {
     super(process);
+    this.opts = {};
   }
 
   exec(params) {
-    var _this = this;
-    _this.bot = new TelegramBot(params.token, { polling: true });
-    _this.opts = {};
-    let chatId = params.chatId;
-    let message = params.message;
-    let confirmButtonMessage = params.confirm_button_message;
-    let cancelButtonMessage = params.cancel_button_message;
+    this.bot = new telegramBot(params.token, { polling: true });
+    const chatId = params.chatId;
+    const message = params.message;
+    const confirmButtonMessage = params.confirm_button_message;
+    const cancelButtonMessage = params.cancel_button_message;
     let confirmMessage = params.confirm_message;
     let cancelMessage = params.cancel_message;
-    var authorizedUsers = params.authorized_users;
-    var authorized = true;
+    const authorizedUsers = params.authorized_users;
+    let authorized = true;
 
     // Keyboard buttons with the options
-    var options = {
+    const options = {
       reply_markup: JSON.stringify({
         inline_keyboard: [
-          [{ text: confirmButtonMessage, callback_data: "1" }],
-          [{ text: cancelButtonMessage, callback_data: "2" }]
+          [{ text: confirmButtonMessage, callback_data: '1' }],
+          [{ text: cancelButtonMessage, callback_data: '2' }]
         ]
       })
     };
 
     //Send a message with the process name.
-    _this.bot.sendMessage(chatId, message, options).then(function (sended) {
-      _this.opts = {
+    this.bot.sendMessage(chatId, message, options).then(sended => {
+      this.opts = {
         chat_id: chatId,
-        message_id: sended.message_id,
+        message_id: sended.message_id
       };
     });
 
     // Start polling waiting for the answer
-    _this.bot.on("callback_query", function (msg) {
-      let back_message_id = msg.message.message_id;
+    this.bot.on('callback_query', msg => {
+      const back_message_id = msg.message.message_id;
 
       // Checks if the user who answered is in the authorized list if the param is received
       if (authorizedUsers && authorizedUsers.length) {
         authorized = false;
-        for (var i = 0; i < authorizedUsers.length; i++) {
+        for (let i = 0; i < authorizedUsers.length; i++) {
           if (authorizedUsers[i].name == msg.from.first_name || authorizedUsers[i].telegramId == msg.from.id) {
             authorized = true;
           }
@@ -55,90 +54,88 @@ class confirmExecutor extends Execution {
       }
 
       // check if the response belongs to the same messageId
-      if (back_message_id == _this.opts.message_id) {
+      if (back_message_id == this.opts.message_id) {
         if (authorized) {
-          if (msg.data == "1") {
-
+          if (msg.data == '1') {
             //Send the confirmation message with the name of the user who confirmed
-            confirmMessage = confirmMessage + " - " + msg.from.first_name + " " + msg.from.last_name;
-            _this.bot.sendMessage(chatId, confirmMessage);
+            confirmMessage = confirmMessage + ' - ' + msg.from.first_name + ' ' + msg.from.last_name;
+            this.bot.sendMessage(chatId, confirmMessage);
 
             //Stop polling
-            _this.bot.stopPolling();
+            this.bot.stopPolling();
 
             //Edit the message erasing the buttons
-            _this.bot.editMessageText(message, _this.opts);
+            this.bot.editMessageText(message, this.opts);
 
             //Process end ok
-            _this.end();
+            this.end();
           } else {
-
-            let endOptions = {
-              end: "error",
-              messageLog: "Chain stop forced by user " + msg.from.first_name + " " + msg.from.last_name,
-              err_output: "Chain stop forced by user " + msg.from.first_name + " " + msg.from.last_name
+            const endOptions = {
+              end: 'error',
+              messageLog: 'Chain stop forced by user ' + msg.from.first_name + ' ' + msg.from.last_name,
+              err_output: 'Chain stop forced by user ' + msg.from.first_name + ' ' + msg.from.last_name
             };
 
             //Send the cancellation message with the name of the user who cancelled
-            cancelMessage = cancelMessage + " - " + msg.from.first_name + " " + msg.from.last_name;
-            _this.bot.sendMessage(chatId, cancelMessage);
+            cancelMessage = cancelMessage + ' - ' + msg.from.first_name + ' ' + msg.from.last_name;
+            this.bot.sendMessage(chatId, cancelMessage);
 
             //Stop polling
-            _this.bot.stopPolling();
+            this.bot.stopPolling();
 
             //Edit the message erasing the buttons
-            _this.bot.editMessageText(message, _this.opts);
+            this.bot.editMessageText(message, this.opts);
 
             //Process end error
-            _this.end(endOptions);
+            this.end(endOptions);
           }
         } else {
-          let unauthorizedMessage = "Sorry " + msg.from.first_name + ", you are not authorized for the action";
-          _this.bot.sendMessage(chatId, unauthorizedMessage);
+          const unauthorizedMessage = 'Sorry ' + msg.from.first_name + ', you are not authorized for the action';
+          this.bot.sendMessage(chatId, unauthorizedMessage);
         }
       }
     });
   }
 
   kill(params, reason) {
-    var _this = this;
-    let chatId = params.chatId;
-    let message = params.message;
-    let action_on_timeout = params.action_on_timeout;
+    const chatId = params.chatId;
+    const message = params.message;
+    const action_on_timeout = params.action_on_timeout;
 
     // If the reason is timeout and the action_on_timeout is ok the process ends ok
-    if (reason == "timeout" && action_on_timeout == "ok"){
-      _this.bot.stopPolling();
+    if (reason == 'timeout' && action_on_timeout == 'ok') {
+      this.bot.stopPolling();
 
       //Send the confirmation message with the name of the user who confirmed
-      let confirmMessage = "Process timeout, action on timeout: " + action_on_timeout + ", the chain continues";
-      _this.bot.sendMessage(chatId, confirmMessage);
+      const confirmMessage = 'Process timeout, action on timeout: ' + action_on_timeout + ', the chain continues';
+      this.bot.sendMessage(chatId, confirmMessage);
 
       //Edit the message erasing the buttons
-      _this.bot.editMessageText(message, _this.opts);
+      this.bot.editMessageText(message, this.opts);
 
       //Process end ok*/
-      _this.end();
-    }else{
+      this.end();
+    } else {
       // Any other reason or no action defined on timeout the process ends with error
-      let endOptions = {
-        end: "error",
-        messageLog: "Process killed, reason: " + reason,
-        err_output: "Process killed, reason: " + reason
+      const endOptions = {
+        end: 'error',
+        messageLog: 'Process killed, reason: ' + reason,
+        err_output: 'Process killed, reason: ' + reason
       };
 
       //Stop polling
-      _this.bot.stopPolling();
+      this.bot.stopPolling();
 
       //Edit the message erasing the buttons
-      _this.bot.editMessageText(message, _this.opts);
+      this.bot.editMessageText(message, this.opts);
 
       //Send the cancellation message with the name of the user who cancelled
-      let cancelMessage = "Procces killed, reason: " + reason + ", acation on timeout: "+ action_on_timeout + ", the chain stops.";
-      _this.bot.sendMessage(chatId, cancelMessage);
+      const cancelMessage =
+        'Procces killed, reason: ' + reason + ', action on timeout: ' + action_on_timeout + ', the chain stops.';
+      this.bot.sendMessage(chatId, cancelMessage);
 
       //Process end error
-      _this.end(endOptions);
+      this.end(endOptions);
     }
   }
 }
